fdd8db97421d05e6379a3e399197d44739582c13
Trevor Jarzynka <trevorjarzynka@Trevors-MacBook-Air.local>
2024-02-14 12:05:16 -0500

    just some comments

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevents the form from submitting the traditional way
+    event.preventDefault(); 
     alert('Thank you for your message!');
-    // Here you would typically handle the form submission (e.g., via AJAX to a server)
-    
+    //Handle the form submission 
 });
 
 
